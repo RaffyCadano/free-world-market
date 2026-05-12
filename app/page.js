98a074c22e71e6f730d9f865fc5a9357eedc6582
RaffyCadano <helloworldcs09@gmail.com>
2026-05-12 01:06:46 -0400

    add cursor effects and import necessary hooks in Page component

--- app/page.js
+++ app/page.js
@@ -1,3 +1,6 @@
+'use client';
+
+import { useEffect, useRef } from 'react';
 import Services from './sections/Services';
 import Approach from './sections/Approach';
 import Problem from './sections/Problem';
@@ -85,13 +88,14 @@ export default function Page() {
         </div>
       </section>
 
-      <Marquee />
-
       <Problem />
 
       <Approach />
       
       <Services />
+
+      <div className="cursor-dot" ref={cursorDot} />
+      <div className="cursor-ring" ref={cursorRing} />
     </>
   );
 }
